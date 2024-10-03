cd3ad46502c1e6aac1d8582a91e8af686361a55e
DianaP2 <L24550326@outlook.com>
2024-10-03 17:46:12 -0600

    Add files via upload

---
+++ EVA2_3_POTENCIA/src/eva2_3_potencia/EVA2_3_POTENCIA.java
@@ -0,0 +1,34 @@
+package eva2_3_potencia;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @Diana Sanchez
+ */
+
+public class EVA2_3_POTENCIA {
+
+    public static void main(String[] args) {
+       
+        int base,expo;
+        Scanner captu=new Scanner(System.in);
+        
+        System.out.println("Introduce la base");
+        base = captu.nextInt();
+        captu.nextLine();
+        
+        System.out.println("Introduce el exponente");
+        expo=captu.nextInt();
+        captu.nextLine();
+        //System.out.println(" Potencia = " + Math.pow(base,expo)
+        
+        int acum = 1;
+        for(int i = 1; i <= expo; i++){
+            acum = acum * base; 
+        }
+        System.out.println("Potencia: " + acum);
+        
+    }
+    
+}
